4cce5f3258dc6fcd55b752b5b5087f57c8d3342a
Damith Premasiri <damithpremasiri@gmail.com>
2024-04-21 17:50:59 +0100

    testing

--- experiment/open_llms_pipe.py
+++ experiment/open_llms_pipe.py
@@ -13,7 +13,7 @@ text_generator = pipeline(
         trust_remote_code=True,
         device_map="auto",
         do_sample=True,
-        max_new_tokens=50,
+        max_new_tokens=200,
         # batch_size=args.batch_size,
         top_k=20,
         num_return_sequences=1,
@@ -31,7 +31,7 @@ prompt = """
     In other words, metaphoric flower name is one that uses a word or phrase that evokes certain qualities, characteristics, or associations beyond the literal description of the flower itself.
     For example, the flower name "Forget-me-not" is metaphorical. While it directly refers to the tiny, delicate blue flowers of the Myosotis genus, its name carries emotional connotations beyond its physical appearance. "Forget-me-not" suggests remembrance, loyalty, and enduring love. It metaphorically implies that the giver of the flower is asking the recipient not to forget them and to remember the bond they share, making it a poignant and symbolic name for this charming flower.
     Your task is to identify metaphorical flower names in the given English text.
-    You must identify if there are metaphoric flower names respond as a json format of yes/no (depending on there is a metaphorical flower name or not) and the list of names in a json object.
+    You must identify if there are metaphoric flower names respond as a json object of yes/no (depending on there is a metaphorical flower name or not) and the list of names in a json object.
     Example json object : {metaphoric_names_found : 'yes',metaphoric_names = ['name1','name2']}
     Do not provide any other explanation. Just return json object with the results.
     
