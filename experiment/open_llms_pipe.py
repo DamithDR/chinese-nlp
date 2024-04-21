2f263b694d8eba3defe55db3cd785dc36eebe73d
Damith Premasiri <damithpremasiri@gmail.com>
2024-04-21 17:38:59 +0100

    testing

--- experiment/open_llms_pipe.py
+++ experiment/open_llms_pipe.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 model_name = "meta-llama/Llama-2-13b-chat-hf"
 
 # Create the text generation pipeline
-text_generator = pipeline("text-generation", model=model_name)
+text_generator = pipeline("text-generation", model=model_name,device_map='auto')
 
 # Generate text using the pipeline
 
@@ -24,6 +24,6 @@ prompt = """
 
 print(torch.cuda.device_count())
 
-text = text_generator(prompt, max_new_tokens=200, do_sample=True, device=0)[0]['generated_text']
+text = text_generator(prompt, max_new_tokens=200, do_sample=True)[0]['generated_text']
 
 print(text)
